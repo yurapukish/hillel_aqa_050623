3ee9847da9791c96d7962be3db6260ebe10dc60e
antohaUa <antoha.ua@gmail.com>
2023-06-05 21:49:43 +0300

    Lesson_01 Hometask01

---
+++ Lesson_01/hometask/l01_t01_str_format.py
@@ -0,0 +1,10 @@
+# we have norway text in old style formatting
+# re-write the same text as:
+# #1 string with format() call
+# #2 f-string
+# use linter(https://github.com/wemake-services/wemake-python-styleguide)
+# to check your new created python module for possible linter errors
+# try to run code from pycharm/command line
+
+norway_text = 'Automatisering akselererer %syeblikket da roboter vil erobre planeten v%sr. (%s)' % ('ø', 'å', 'Æ')
+print(norway_text)
