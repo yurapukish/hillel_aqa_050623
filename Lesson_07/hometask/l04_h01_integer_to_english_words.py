c226e03bf3ab5428b696f3d80b97b193cfbe5ec5
antohaUa <antoha.ua@gmail.com>
2023-06-26 22:06:42 +0300

    Lesson_07 Hometask

---
+++ Lesson_07/hometask/l04_h01_integer_to_english_words.py
@@ -0,0 +1,6 @@
+# Convert a non-negative integer num to its English words representation.
+# Example 1:
+# Input: num = 123
+# Output: "One Hundred Twenty Three"
+
+# let's take that number always > 100
