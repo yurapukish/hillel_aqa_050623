6c2c808babd59f291ea39cb35dd6aecaeaa7a1f2
antohaUa <antoha.ua@gmail.com>
2023-06-29 21:35:23 +0300

    Lesson_08 Practice Hometask

---
+++ Lesson_08/hometask/l08_h01_max_three_multiplication.py
@@ -0,0 +1,9 @@
+# Given the list of integers ( positive , negative, zeros)
+# Find max multiplication of three values in list
+l1 = [-7, 1, 2, 0, 3, 12, -24, 5, -1, 6]
+
+# Input: [-1, 1, 2, 0, 3, 12, 4, 5, -1, 6]
+# Output: Max value = "360". Nums are: (12, 5, 6)
+
+# Input: [-7, 1, 2, 0, 3, 12, -24, 5, -1, 6]
+# Output: Max value = "2016". Nums are: (-7, 12, -24)
