60b1f4a87304cd0c39426a9481144a0874ead96b
antohaUa <antoha.ua@gmail.com>
2023-07-03 21:42:41 +0300

    Lesson_09 Hometask

---
+++ Lesson_09/hometask/l09_h01_read_write_files.py
@@ -0,0 +1,9 @@
+# write script that will read "input.txt" file and find there all characters from english alphabet
+# collect these characters and their positions in file
+# after info collected this info as text write to "output.txt" file in such format
+# ####################
+# <first found letter> -> pos1
+# <next_letter> -> pos2
+# <next_letter -> pos3
+# etc
+# #######################
