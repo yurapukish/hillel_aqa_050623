e0014926b3dbec240a2025d8a4e2da948192b34b
antohaUa <antoha.ua@gmail.com>
2023-07-06 21:53:13 +0300

    Lesson_10 Hometask

---
+++ Lesson_10/hometask/l10_h01_regex.py
@@ -0,0 +1,8 @@
+# open input.txt file and find all small english letters that match such conditions:
+# target small letter round exactly with three capital english letters on the left and on the right
+# Match examples:
+# sdTRYaUVKn  -> matches "a"
+# NTRSjARTb   -> no match ( not exactly 3 capital letters on the left)
+# zDFGbOPNq   -> matches "b"
+# Print Output as : "Result: "<your_found_human_readable_string">
+
