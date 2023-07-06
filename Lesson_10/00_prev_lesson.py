3a3c33e98255a887fbbb1596db1f8abe9d078cc6
antohaUa <antoha.ua@gmail.com>
2023-07-06 21:19:15 +0300

    Lesson_10 Regex

---
+++ Lesson_10/00_prev_lesson.py
@@ -0,0 +1,30 @@
+# Please do not use itertools in hometask 08
+
+l4 = [-1, 0, 1, -12]
+from itertools import combinations
+
+l_comb = list(combinations(l4, 3))
+print(l_comb)
+
+
+# git branches
+
+if True:
+    val = 1
+else:
+    val = '34242'
+
+print(val)
+
+
+
+def funct(x):
+    #....
+    return val
+
+result = funct('2123')
+print()
+
+# data
+
+
