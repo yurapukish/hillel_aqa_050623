ac4425f71d161eef018be08c1fe1cb2877dc42e3
antohaUa <antoha.ua@gmail.com>
2023-07-13 21:42:43 +0300

    Lesson_12 Hometask

---
+++ Lesson_12/hometask/l12_h01_recursive_function.py
@@ -0,0 +1,5 @@
+# Given list of list of list etc of integers
+# write recursive function that will accept as argument target list
+# and return sum of all integers inside it
+# Input: [[[[1, 4, 5], [[6, 9],[[[8, 1], 7], 3], 2], 7], 5, 2], 9, [1, 2]]
+# Output: Target sum = 72
