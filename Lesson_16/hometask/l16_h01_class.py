c6a8fcdee10aa62a66265965bf0d5e6a1b0ca147
antohaUa <antoha.ua@gmail.com>
2023-08-03 21:36:09 +0300

    Lesson_16 Classes Hometask

---
+++ Lesson_16/hometask/l16_h01_class.py
@@ -0,0 +1,6 @@
+# write class for bank card.
+# Class should reflect card lifecycle, card operations etc.
+# use CVV, PIN, Name, Surname , end date, card_num as initial params
+# class should have in addition to common logic some class attributes, as minimum one classmethod and
+# as minimum  one staticmethod, two or more getters/setters
+# do not forget about block """if __name__ == '__main__':"""
