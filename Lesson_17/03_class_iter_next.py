5ba5cb1deb401f45e86b8d625bb8cc598afc3223
antohaUa <antoha.ua@gmail.com>
2023-08-07 21:30:34 +0300

    __next__ correction

--- Lesson_17/03_class_iter_next.py
+++ Lesson_17/03_class_iter_next.py
@@ -11,21 +11,18 @@ class EnglishLetters:
 
     def __next__(self):
         self.pos += 1
-        # print(self.pos)
-        # if self.pos == 52:
-        #     breakpoint()
+        if self.pos == len(self.letters):
+            raise StopIteration
         return self.letters[self.pos]
 
 
-
 el_iter = EnglishLetters()
-print(next(el_iter))
-print(next(el_iter))
-print(next(el_iter))
+# print(next(el_iter))
+# print(next(el_iter))
+# print(next(el_iter))
 
-# # for itm in el_iter:
-# #     print(itm)
 #
-# print(*[itm for itm in el_iter], sep='\n')
+# for itm in el_iter:
+#     print(itm)
 
-
+print(*[itm for itm in el_iter], sep=' ')
